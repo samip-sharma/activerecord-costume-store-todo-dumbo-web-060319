f075c80da0273c5ef750ea9f7ab22c331e4d141b
Samip Sharma <samip.sharma963@gmail.com>
2019-06-15 21:39:41 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,6 +3,13 @@
 class HauntedHouses < ActiveRecord::Migration
   def change
     create_table :houses do |t|
+       t.string :name
+            t.string :name
+            t.integer :costume_inventory
+            t.integer :num_of_employees
+            t.string  :still_in_business
+            t.datetime :opening_time
+            t.datetime :closing_time
       
     end
   end
