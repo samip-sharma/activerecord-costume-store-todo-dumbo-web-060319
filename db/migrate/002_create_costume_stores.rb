2ec5019395ae71361c08e1872e5ef0e208476186
Samip Sharma <samip.sharma963@gmail.com>
2019-06-15 21:32:24 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,10 +1,4 @@
 # Create your costume_stores migration here   t.string :name
-      # t.string :location
-      # t.integer :number_of_costumes
-      # t.integer :number_of_employees
-      # t.string  :status
-      # t.timestamps
-      
       
       class CostumeStore < ActiveRecord::Migration[4.2]
         def change
