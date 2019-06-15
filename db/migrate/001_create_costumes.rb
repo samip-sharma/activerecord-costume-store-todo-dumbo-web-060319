f6723a7cfb2b975a0b27f3c37a2def852f8aa298
Samip Sharma <samip.sharma963@gmail.com>
2019-06-15 21:14:27 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,6 +12,9 @@ class CreateCostumes <ActiveRecord::Migrate
   def change
     create_table :costumes do |t|
    t.string :name
+   t,integer :price
+   t.integer :size 
+   t.string :url
     end
   end
 end
