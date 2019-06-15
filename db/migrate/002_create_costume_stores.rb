7aef1e0e0fb72d3e50255b4864bcfe553ca3d481
Samip Sharma <samip.sharma963@gmail.com>
2019-06-15 21:13:19 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,6 @@
-# Create your costume_stores migration here
+# Create your costume_stores migration here   t.string :name
+      # t.string :location
+      # t.integer :number_of_costumes
+      # t.integer :number_of_employees
+      # t.string  :status
+      # t.timestamps
